6082202c991ca4e1ea1cd7bb56c5dcca71edb8dc
fahadaleem <faleem396@gmail.com>
2021-11-25 00:19:37 +0500

    redux toolkit setup, auth reducer created.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 /* eslint-disable */
 import "./App.css";
 import Layouts from "./layouts/routes";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
+
 function App() {
-  return <Layouts />;
+  return (
+    <Provider store={store}>
+      <Layouts />
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/store/authSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    isAuthenticated: false,
+    user: null,
+    token: null,
+    error: null,
+  },
+  reducers: {
+    login: (state, action) => {
+      state.isAuthenticated = true;
+      state.user = action.payload.user;
+      state.token = action.payload.token;
+    },
+    logout: (state) => {
+      state.isAuthenticated = false;
+      state.user = null;
+      state.token = null;
+    },
+  },
+});
+
+const { login, logout } = authSlice.actions;
+export default authSlice.reducer;
